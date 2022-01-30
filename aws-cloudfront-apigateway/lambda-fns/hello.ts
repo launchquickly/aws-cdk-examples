0f22e95b7d2216e0340d7b38f809c4e9c190a654
pauljkearney <hello@launchquickly.scot>
2022-01-30 20:12:21 +0000

    Changed lambda function from js to ts

---
+++ aws-cloudfront-apigateway/lambda-fns/hello.ts
@@ -0,0 +1,18 @@
+export {};
+
+exports.handler = async (event:any) => {
+  console.log(JSON.stringify(event, null, 2));
+
+  return sendRes(200, `Hello, CDK! You've hit ${event.path}\n`);
+}
+
+const sendRes = (status:number, body:string) => {
+  var response = {
+    statusCode: status,
+    headers: {
+      "Content-Type": "text/plain"
+    },
+    body: body
+  };
+  return response;
+};
